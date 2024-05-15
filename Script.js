bdbd7431dc06ebb42db5002ea955a92c1cc25f99
Anwarul07 <mdanwarul064@gmail.com>
2024-05-15 22:22:26 +0530

    created script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
-console.log("Quite can change your life ")
+console.log("Quite can change your life ");
+
+const quite = document.getElementById("quite");
+const author = document.getElementById("author");
+const api = "https://api.quotable.io/random";
+async function getquite(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    quite.innerHTML= data.content;
+    author.innerHTML= data.author;
+}
+
+getquite(api);
