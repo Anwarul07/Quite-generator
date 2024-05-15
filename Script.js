1273d36d9687a85fa41d70c061b8739b6f63aff7
Anwarul07 <mdanwarul064@gmail.com>
2024-05-15 22:37:06 +0530

    update script.js

--- Script.js
+++ Script.js
@@ -7,8 +7,12 @@ async function getquite(url) {
     const response = await fetch(url);
     var data = await response.json();
     console.log(data);
-    quite.innerHTML= data.content;
-    author.innerHTML= data.author;
+    quite.innerHTML = data.content;
+    author.innerHTML = data.author;
 }
 
 getquite(api);
+
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text= " + quite.innerHTML + "      ----by  " + author.innerHTML, "tweet window", "width=600, height=300")
+}
